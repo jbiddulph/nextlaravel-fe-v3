9c45b43175de5aafa070d18bebf58b4f81a1f52c
John Biddulph <john.mbiddulph@gmail.com>
2025-04-04 17:38:42 +0100

    redirect setup for www.goschool.uk to go to goschool.uk

--- next.config.js
+++ next.config.js
@@ -16,6 +16,21 @@ const baseConfig = {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN: process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN,
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*", // Match all paths
+        has: [
+          {
+            type: "host",
+            value: "goschool.uk", // Redirect only if the host is goschool.uk
+          },
+        ],
+        destination: "https://www.goschool.uk/:path*", // Redirect to www.goschool.uk
+        permanent: true, // 301 redirect
+      },
+    ];
+  },
   webpack(config, { isServer }) {
     if (!isServer) {
       // Exclude 'child_process' from client-side bundle
